a37f98e8b426585157de8352d2602cc4aaf1e785
hobak12 <tjsgud2564@naver.com>
2022-11-22 10:00:17 +0900

    kimnamgyu branch clone

--- js/firebase.js
+++ js/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";   // 사용안함
 
 // 아래 데이터는 본인의 Firebase 프로젝트 설정에서 확인할 수 있습니다.
 const firebaseConfig = {
@@ -12,7 +13,7 @@ const firebaseConfig = {
   storageBucket: "i-like-people.appspot.com",
   messagingSenderId: "671297245671",
   appId: "1:671297245671:web:971b3c4bea9c579d0bed42",
-  measurementId: "G-Y9J7QEH4D8"
+  measurementId: "G-Y9J7QEH4D8"   // 사용안함
 };
 
 // Initialize Firebase
@@ -20,3 +21,4 @@ export const app = initializeApp(firebaseConfig);
 export const dbService = getFirestore(app);
 export const authService = getAuth(app);
 export const storageService = getStorage(app);
+export const analytics = getAnalytics(app);   // 사용안함
